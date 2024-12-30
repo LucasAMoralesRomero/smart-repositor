4b3d03b0ca4f69ff04c1c76a76e1dd6ab02622cb
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2024-12-30 20:05:57 -0300

    Actualizar js
    Se crea archivo para mostrar novedades dinamicamente

---
+++ js/activity.js
@@ -0,0 +1,19 @@
+document.getElementById("showProgress").addEventListener("click", function() {
+  const progressList = document.getElementById("progressList");
+  const tasks = [
+    "27/12 - Se detecta falla en el sistema, se pone el mismo en modo mantenimiento 👷‍♂️",
+    "28/12 - Comienza el trabajo en el  servidor (smart-repositor-server) 🛠️",
+    "29/12 - Comienzan las pruebas del servidor en entornos controlados 🧪"];
+
+  // Limpiar la lista si ya fue cargada
+  progressList.innerHTML = "";
+
+  // Simular tiempo real agregando cada tarea con un retraso
+  tasks.forEach((task, index) => {
+    setTimeout(() => {
+      const listItem = document.createElement("li");
+      listItem.textContent = task;
+      progressList.appendChild(listItem);
+    }, index * 2000); // Agregar tareas cada 2 segundos
+  });
+});
